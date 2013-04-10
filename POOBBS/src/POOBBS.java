846fa16f804f4d2d97de79553e4ce0b77ac35657
Vison Lee <b99902079@ntu.edu.tw>
2013-04-11 00:32:17 +0800

    a method called makeDir

--- POOBBS/src/POOBBS.java
+++ POOBBS/src/POOBBS.java
@@ -1,27 +1,48 @@
 
 public class POOBBS {
 	public static void main(String[] args) {
+		
+		POODirectory[] dir = new POODirectory[100];
+		POOBoard[] board = new POOBoard[100];
+		makeDir(dir);
+		
+		
+		
+		dir[0].show();
+	}
+
+	private static void makeDir(POODirectory[] dir){
 		int numOfDir = 0;
 		int i;
-		POODirectory[] dir = new POODirectory[10];
-		POOBoard[] board = new POOBoard[10];
-		
 		dir[numOfDir++] = new POODirectory("Home");
+		// put in home
 		dir[numOfDir++] = new POODirectory("NTU");
 		dir[numOfDir++] = new POODirectory("NCCU");
 		dir[numOfDir++] = new POODirectory("University");
 		dir[numOfDir++] = new POODirectory("HOT");
+		// put in NTU
 		dir[numOfDir++] = new POODirectory("CSIE");
 		dir[numOfDir++] = new POODirectory("BA");
 		dir[numOfDir++] = new POODirectory("ME");
+		// put in NCCU
+		dir[numOfDir++] = new POODirectory("NCCU_1");
+		dir[numOfDir++] = new POODirectory("NCCU_2");
+		dir[numOfDir++] = new POODirectory("NCCU_3");
+		// put in University
+		dir[numOfDir++] = new POODirectory("NCTU");
+		dir[numOfDir++] = new POODirectory("NCHU");
+		dir[numOfDir++] = new POODirectory("NTNU");
 
 		for(i=0;i<numOfDir;i++)
 			dir[i].init();
-		for(i=0;i<10;i++)
-			board[i] = new POOBoard("board_"+i);
-		for(i=0;i<4;i++)
-			dir[0].add(dir[i+1]);
-		dir[0].show();
+		
+		for(i=1;i<=4;i++)
+			dir[0].add(dir[i]);
+		for(i=5;i<=7;i++)
+			dir[1].add(dir[i]);
+		for(i=8;i<=10;i++)
+			dir[2].add(dir[i]);
+		for(i=11;i<=13;i++)
+			dir[3].add(dir[i]);
 	}
-
 }
