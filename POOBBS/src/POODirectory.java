b0916293fede6e91303187584ca0994918685463
Vison Lee <b99902079@ntu.edu.tw>
2013-04-10 21:20:20 +0800

    add item of Directory

--- POOBBS/src/POODirectory.java
+++ POOBBS/src/POODirectory.java
@@ -1,9 +1,20 @@
+class Item{
+	public String type;
+	public int seq;
+}
+
 public class POODirectory {
-	public  POODirectory(String name){
-		
+	//public String[] type = new String [1024];
+	//public int[] num = new int [1024];
+	public String name ;
+	public Item[] item = new Item[1024];
+	private int count = 0;
+	public  POODirectory(String nametmp){
+		name = nametmp;
 	}
 	public void add(POOBoard board){
-		
+		item[count].type = String.valueOf("board");
+		item[count].seq = board.getSeq();
 	}
 	public void add(POODirectory dir){
 		
